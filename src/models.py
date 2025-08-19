ea377c7f44d07d692bbfc2acbedf7fe943632cc0
cgerc <gercanepacarmen@gmail.com>
2025-08-19 16:39:40 +0000

    agrega tabla favoritos

--- src/models.py
+++ src/models.py
@@ -57,3 +57,21 @@ class User(db.Model):
             "species": self.species,
             "homeworld": self.homeworld.name if self.homeworld else None
         }
+    
+    class Favorite(db.Model):
+        __tablename__ = 'favorite'
+        id: Mapped[int] = mapped_column(primary_key=True)
+        user_id: Mapped[int] = mapped_column(ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+        planet_id: Mapped[int] = mapped_column(ForeignKey('planet.id', ondelete='CASCADE'), nullable=True)
+        character_id: Mapped[int] = mapped_column(ForeignKey('character.id', ondelete='CASCADE'), nullable=True)
+        user: Mapped["User"] = relationship("User", back_populates="favorites")
+        planet: Mapped["Planet"] = relationship("Planet", back_populates="favorites")
+        character: Mapped["Character"] = relationship("Character", back_populates="favorites")
+
+        def serialize(self):
+            return {
+                "id": self.id,
+                "user_id": self.user_id,
+                "planet": self.planet.serialize() if self.planet else None,
+                "character": self.character.serialize() if self.character else None
+        }
