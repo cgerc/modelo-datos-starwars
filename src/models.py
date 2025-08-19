9b497e1e57fa64a94422fd77be3ab6e8c20460de
cgerc <gercanepacarmen@gmail.com>
2025-08-19 16:24:49 +0000

    corregir tipo de dato

--- src/models.py
+++ src/models.py
@@ -6,8 +6,8 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
+    email: Mapped[str] = mapped_column(String , unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String ,nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
     favorites: Mapped[list["Favorite"]]= relationship("Favorite" , back_populates="user")
 
@@ -17,3 +17,7 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    class Planet(db.Model):
+    __tablename__: 'planet'
+    id: Mapped[int] = mapped_column(primary_key= True)
+    name: Mapped[str]= mapped_column(String100)
