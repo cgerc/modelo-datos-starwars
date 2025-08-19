cbd44e182c4c4f6da1d0c1d37a0014f4b98c946a
cgerc <gercanepacarmen@gmail.com>
2025-08-19 16:09:27 +0000

    imports

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer , ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
